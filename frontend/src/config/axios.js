b163c329a55ec9549c4684e90579e76e205046ec
MirzaParvejAhmed <150512019+MirzaParvejAhmed@users.noreply.github.com>
2025-08-03 00:47:11 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,11 +1,26 @@
+
 import axios from 'axios';
+import { UserContext } from '../context/user.context';
+import { useContext } from 'react';
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers:{
-    "Authorization":`Bearer ${localStorage.getItem('token')}`,
+});
 
-  }})
-export default axiosInstance; 
+// Request interceptor to add the token dynamically
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
+export default axiosInstance;
 
 
